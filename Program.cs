8ee6e91c51f25152bcfab2528a7ef6227b3ac718
Trevor Bridgewater <tbridgewater@usf.edu>
2021-09-07 13:54:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_Assignment_1___Simple_Console_App_TB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Happy Fall semester! Wear a mask in crowds and social distance!");
+        }
+    }
+}
