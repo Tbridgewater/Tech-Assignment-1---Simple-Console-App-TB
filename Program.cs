c44aa875f561e8afa6294d229b98321247602700
Trevor Bridgewater <tbridgewater@usf.edu>
2021-09-07 14:05:27 -0400

    added additional message.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Tech_Assignment_1___Simple_Console_App_TB
         static void Main(string[] args)
         {
             Console.WriteLine("Happy Fall semester! Wear a mask in crowds and social distance!");
+            Console.WriteLine("Stop by the MSC for food and drinks.");
         }
     }
 }
